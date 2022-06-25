df5ed53788d17592edbcaa98a4e3abafc1066862
Douyin-vbuser <douyin_vbuser@outlook.com>
2022-06-25 17:52:27 +0800

    release 3.1.0 commit

--- src/main/java/com/vbuser/btnote/util/Reference.java
+++ src/main/java/com/vbuser/btnote/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String Mod_ID = "btnote";
     public static final String NAME = "btnote";
-    public static final String VERSION = "release 3.0.7";
+    public static final String VERSION = "release 3.1.0";
     public static final String ACCEPT_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.vbuser.btnote.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.vbuser.btnote.proxy.CommonProxy";
